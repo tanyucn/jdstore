010074dcd831fb5a792b22b04d65d7316da3a11a
谭宇 <tanyucn@outlook.com>
2017-07-30 19:42:12 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ root 'products#index'
      resources :products
    end
 
-   resources :products
+     resources :products do
+       member do
+         post :add_to_cart
+       end
+     end
 
 end
