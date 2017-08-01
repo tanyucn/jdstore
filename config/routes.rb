90ed2bbbb4537e3727bbb44d2bfadcf8b382c9e3
谭宇 <tanyucn@outlook.com>
2017-08-01 10:36:11 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,14 @@ root 'products#index'
       end
 
       resources :cart_items
-      resources :orders
       
+        resources :orders do
+          member do
+            post :pay_with_alipay
+            post :pay_with_wechat
+          end
+        end
+
        namespace :account do
          resources :orders
        end
