beef51dc6df764eda21cf9d14db593073582d23f
谭宇 <tanyucn@outlook.com>
2017-08-05 23:00:02 +0800

    text

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,27 +17,25 @@ end
  # Initialize Product
 
  Product.create!(title: "MacBook Air",
-   description: "Mac Air",
+   description: "Mac",
    price: 8000,
    quantity: 15,
    image: open("https://ws4.sinaimg.cn/large/006tNc79gy1fi6n7vsuw6j31hc0zkdia.jpg")
    )
 
  Product.create!(title: "MacBook Pro",
-   description: "Mac Pro",
+   description: "Mac",
    price: 12000,
    quantity: 25,
    image: open("https://img.alicdn.com/imgextra/i2/197232874/TB2Y8sjxJBopuFjSZPcXXc9EpXa_!!197232874.jpg")
    )
 
-
  Product.create!(title: "MacBook Pro+",
-   description: "Mac Pro",
+   description: "Mac",
    price: 15000,
    quantity: 5,
    image: open("https://img.alicdn.com/imgextra/i2/1917047079/TB27bfoubplpuFjSspiXXcdfFXa_!!1917047079.jpg")
    )
-
 
 
  Product.create!(title: "book",
